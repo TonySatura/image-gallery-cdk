d63705a91c29fb30b9a3317247f1f3f8534e4dec
TonySatura <tony.satura@awin.com>
2020-08-06 13:59:53 +0200

    cleanup

--- lib/ui.stack.ts
+++ lib/ui.stack.ts
@@ -1,15 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
-import * as s3deploy from "@aws-cdk/aws-s3-deployment";
-import * as route53 from "@aws-cdk/aws-route53";
-import * as acm from "@aws-cdk/aws-certificatemanager";
-import * as route53targets from "@aws-cdk/aws-route53-targets/lib";
-import * as codepipeline from "@aws-cdk/aws-codepipeline";
-import * as codepipelineActions from "@aws-cdk/aws-codepipeline-actions";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
 import { UiStackProps } from "./stack-props";
-import { RemovalPolicy } from "@aws-cdk/core";
-import { PriceClass } from "@aws-cdk/aws-cloudfront";
 
 export class UiStack extends cdk.Stack {
   public readonly siteBucket: s3.Bucket;
@@ -21,25 +13,24 @@ export class UiStack extends cdk.Stack {
     //     domainName: props.rootDomain
     // });
 
-    const siteBucket = new s3.Bucket(this, "site-", {
+    this.siteBucket = new s3.Bucket(this, "site-", {
       blockPublicAccess: {
         blockPublicAcls: true,
         ignorePublicAcls: true,
         blockPublicPolicy: false,
-        restrictPublicBuckets: false
+        restrictPublicBuckets: false,
       },
       websiteIndexDocument: "index.html",
       publicReadAccess: true,
-      removalPolicy: RemovalPolicy.DESTROY
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
-    this.siteBucket = siteBucket;
 
     const siteDistribution = new cloudfront.CloudFrontWebDistribution(
       this,
       "site-distribution",
       {
         comment: "Website distribution for " + props.appName,
-        priceClass: PriceClass.PRICE_CLASS_ALL,
+        priceClass: cloudfront.PriceClass.PRICE_CLASS_ALL,
         // aliasConfiguration: {
         //     acmCertRef: certificateArn,
         //     names: [uiSiteDomain],
@@ -49,30 +40,30 @@ export class UiStack extends cdk.Stack {
         originConfigs: [
           {
             s3OriginSource: {
-              s3BucketSource: siteBucket
+              s3BucketSource: this.siteBucket,
             },
-            behaviors: [{ isDefaultBehavior: true }]
-          }
+            behaviors: [{ isDefaultBehavior: true }],
+          },
         ],
         errorConfigurations: [
           {
             errorCode: 403,
             errorCachingMinTtl: 300,
             responsePagePath: "/index.html",
-            responseCode: 200
+            responseCode: 200,
           },
           {
             errorCode: 404,
             errorCachingMinTtl: 300,
             responsePagePath: "/index.html",
-            responseCode: 200
-          }
-        ]
+            responseCode: 200,
+          },
+        ],
       }
     );
 
     new cdk.CfnOutput(this, "SiteURL", {
-      value: "https://" + siteDistribution.domainName
+      value: "https://" + siteDistribution.domainName,
     });
   }
 }
